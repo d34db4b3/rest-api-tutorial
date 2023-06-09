008c7c6b7c003bd39f8b211c5b9713bb56ba1160
d34db4b3 <thomas.potier@outlook.com>
2023-06-10 00:42:52 +0200

    add: greetings

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
-use axum::{routing::get, Router};
+use axum::{extract::Query, routing::get, Json, Router};
+use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
+
+#[derive(Deserialize)]
+struct GreetQuery {
+    name: Option<String>,
+}
+
+#[derive(Serialize)]
+struct GreetResponse {
+    greeting: String,
+}
+
+async fn greet(greet_query: Query<GreetQuery>) -> Json<GreetResponse> {
+    Json(GreetResponse {
+        greeting: format!(
+            "Hello, {}!",
+            greet_query
+                .name
+                .as_ref()
+                .map(String::as_str)
+                .unwrap_or("Anonymous")
+        ),
+    })
+}
 
 #[tokio::main]
 async fn main() {
-    // register a "helloworld" handler to the `/` route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    // register our "greeting" handler to the `/greet` route
+    let app = Router::new().route("/greet", get(greet));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on http://{}", addr);
