a0c21e95aae87ea0ae7bbdd4f3a79ccb0f5824db
d34db4b3 <d34db4b3@protonmail.com>
2023-06-14 22:43:22 +0200

    add: start of errors

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,65 @@
-use axum::{extract::Query, routing::get, Json, Router};
-use serde::{Deserialize, Serialize};
-use std::net::SocketAddr;
+use axum::{
+    http::{self, HeaderValue, Response, StatusCode},
+    routing::get,
+    Router,
+};
+use std::{any::Any, net::SocketAddr, ptr};
+use tower_http::catch_panic::{CatchPanicLayer, ResponseForPanic};
 
-#[derive(Deserialize)]
-struct GreetQuery {
-    name: Option<String>,
+async fn segmentation_fault() {
+    unsafe {
+        let _ = ptr::null::<u8>().read();
+    }
+    unreachable!()
 }
 
-#[derive(Serialize)]
-struct GreetResponse {
-    greeting: String,
+async fn sig_kill() {
+    nix::sys::signal::kill(nix::unistd::getpid(), nix::sys::signal::SIGKILL).unwrap();
+    unreachable!()
 }
 
-async fn greet(greet_query: Query<GreetQuery>) -> Json<GreetResponse> {
-    Json(GreetResponse {
-        greeting: format!(
-            "Hello, {}!",
-            greet_query
-                .name
-                .as_ref()
-                .map(String::as_str)
-                .unwrap_or("Anonymous")
-        ),
-    })
+async fn panic() {
+    panic!()
+}
+
+#[derive(Clone)]
+struct PanicHandler;
+
+impl ResponseForPanic for PanicHandler {
+    type ResponseBody = String;
+
+    fn response_for_panic(
+        &mut self,
+        err: Box<dyn Any + Send + 'static>,
+    ) -> Response<Self::ResponseBody> {
+        let mut res = Response::new(format!(
+            "Panic: {}",
+            if let Some(s) = err.downcast_ref::<String>() {
+                s.as_str()
+            } else if let Some(s) = err.downcast_ref::<&str>() {
+                s
+            } else {
+                "???"
+            }
+        ));
+        *res.status_mut() = StatusCode::INTERNAL_SERVER_ERROR;
+
+        const TEXT_PLAIN: HeaderValue = HeaderValue::from_static("text/plain; charset=utf-8");
+        res.headers_mut()
+            .insert(http::header::CONTENT_TYPE, TEXT_PLAIN);
+
+        res
+    }
 }
 
 #[tokio::main]
 async fn main() {
     // register our "greeting" handler to the `/greet` route
-    let app = Router::new().route("/greet", get(greet));
+    let app = Router::new()
+        .route("/panic", get(panic))
+        .route("/sigkill", get(sig_kill))
+        .route("/segfault", get(segmentation_fault))
+        .layer(CatchPanicLayer::custom(PanicHandler));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on http://{}", addr);
